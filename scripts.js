c1b1dcf022f5f62431e4e03654bc77de7ac4e67e
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-25 19:12:22 +0600

    add text

---
+++ scripts.js
@@ -0,0 +1,107 @@
+// let number = 1;
+// while (number <= 5){
+//     console.log(number);
+//     number++;
+// }
+//secle
+// let number1 = 5;
+// while (number1 >= 1){
+//     console.log(number1);
+//     number1--;
+//
+// let number2 = 1;
+// while (number2 <= 20){
+//     if(number2 % 3 == 0){
+//         console.log(number2);
+
+//     }
+//     number2++;
+// }
+// let number2 = 1;
+// while (number2 <= 20){
+//     if(number2 % 2 == 0){
+//         console.log(number2);
+
+//     }
+//     number2++;
+// }
+// let number2 = 1;
+// while (number2 <= 20){
+//     if(number2 % 2 == 1){
+//         console.log(number2);
+
+//     }
+//     number2++;
+// }
+//99.99
+// let number2 = 0;
+// while (number2 < 20){
+//         console.log(number2);
+
+//     number2++;
+// // }
+// let namber2 = 0;
+// do{
+//     console.log(namber2);
+//     namber2++;
+
+// }while (number2 < 3)
+// let number3 = 0;
+// while(number3 < 3){
+//     console.log(number3 * number);
+//     number3++;
+
+
+// number++;
+// let number2 = 0;
+// while (number2 < 5){
+//     console.log(number2)
+//     ;
+//     numnber2++;
+// /
+// let number2 = 0;
+
+// do{
+
+//         console.log(number2);
+//         number2++;
+// ;
+// }
+// while (number2 < 3);
+// for (let number3 = 5; number3 <= 1; number3--) {
+//     console.log(number3);
+// }
+// for (let number2 = 0;number2 = 10){
+//     if{
+
+//     }
+// }
+//  let number = 0;
+//  while (true){
+
+//     number = prompt("hdsjkjskjhsdksd");
+//     if( +number < 100){
+//         break;
+//     }
+//  }
+
+// document.write("sdjkjfjkjhjj " + number);
+// let number2 = 1;
+// while (number2 <= 10){
+//     if(number2 % 2 == 0){
+//         console.log(number2);
+//     }
+//     number2++;
+// }
+
+// let number2 = 1;
+// while (number2 <= 10){
+//     if(number2 % 2 == 0){
+//         console.log(number2);
+//     }
+//     number2++;
+// }
+for (let number2 = 1;number2 <= 10;number2++){
+    if(number2 % 2 != 0)
+continue;
+console.log(number2);}
